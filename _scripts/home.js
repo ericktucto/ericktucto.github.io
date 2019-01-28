4847311d8b64c88af59c14af07c1aed3d37a8b4c
Erick Tucto <erick.tucto@outlook.com>
2019-01-28 18:40:35 -0500

    Fix mobile media
    Changes to be committed:
        modified:   _includes/header.html
        modified:   _sass/pages/home.sass
        modified:   _sass/pages/post.sass
        modified:   _sass/partials/nav.sass
        modified:   _scripts/home.js
        modified:   assets/css/404.sass
        modified:   assets/js/404.js
        modified:   assets/js/home.js
        modified:   assets/js/main.js
        modified:   assets/js/post.js
        modified:   package.json

--- _scripts/home.js
+++ _scripts/home.js
@@ -1,11 +1,20 @@
 import $ from 'jquery'
 
 $(document).ready((e) => {
-    // Efecto Parallax y cambiar color del nav
+    // ------ Esto es para adaptar el nav y el header
+    let mobile = () => {
+        $("#header").css({
+            'margin-top': $("#nav").height()
+        })
+    }
+    let mediaquery = window.matchMedia("(min-width: 1024px)")
+    mediaquery.addListener(mobile)
+    // ------
+    // ------ Efecto Parallax y cambiar color del nav
     $(window).on('scroll', (e) => {
-        let scroll = e.pageY / 5
+        let scroll = window.pageYOffset / 5
         if (scroll < 100)
             $('#header').css('background-position', `center ${scroll}%`)
-            $('#nav').css('background', `rgba(255, 255, 255, ${scroll / 50})`)
     })
+    // ------
 })
