dbeb8f76ccbde355c8f16fe0946d05ea7a326561
Erick Tucto <erick.tucto@outlook.com>
2019-07-10 18:46:08 -0500

    Update: NavBar Simplify

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,23 +1,5 @@
 import '../_sass/libs/stix-grid/es6/main.es6'
-import $ from 'jquery'
 import '@webcomponents/webcomponentsjs'
 import './src/components/TabLink'
 import './src/components/SocialLink'
 import './src/components/NavBar'
-import './src/components/NavButtonMenu'
-
-$(document).ready((e) => {
-    let items = $("#social-header, #links")
-    // items.each((i, item) => {
-    //     $(item).addClass('hide-until-tablet')
-    // })
-    $("#menu").click(() => {
-        items.each((i, item) => {
-            if ($(item).hasClass('hide-until-tablet')) {
-                $(item).removeClass('hide-until-tablet')
-            } else {
-                $(item).addClass('hide-until-tablet')
-            }
-        })
-    })
-});
--- _scripts/src/components/NavBar.js
+++ _scripts/src/components/NavBar.js
@@ -1,21 +1,18 @@
-import { html, define, children } from 'hybrids'
+import { html, define } from 'hybrids'
 import styles from './NavBar.sass'
-import NavButtonMenu from './NavButtonMenu'
 
-const onPanel = (host, { detail: { show } }) => { host.show = show }
-
-const template = ({show}) => html`
-  <style>${styles}</style>
-  <nav-button-menu onpanel="${onPanel}">
-    <slot name="icon-menu"></slot>
-  </nav-button-menu>
-  ${show && html`<slot id="content"></slot>`}
-`.define({ NavButtonMenu })
+const toggle = (host) => { host.hidden = !host.hidden }
 
 const NavBar = {
-  menu: children(NavButtonMenu),
-  show: false,
-  render: template
+  hidden: true,
+  render: ({ hidden }) => html`
+  <div class="full">
+    <div id="menu" onclick="${toggle}">
+      <slot name="icon-menu"></slot>
+    </div>
+  </div>
+  <slot class="${{ hidden } }"></slot>
+`.style(styles)
 }
 
 export default define('nav-bar', NavBar)
